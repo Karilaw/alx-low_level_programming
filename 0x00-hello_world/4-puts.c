ec24c2418d4066054b60754c34fee72441f6c978
Karilaw <lawrencekariithi6@gmail.com>
2023-03-16 13:56:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - print one line  followed by a new line using put fuction.
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
