6d579c1d611ced11401acab44acb6047e1027c06
Karilaw <lawrencekariithi6@gmail.com>
2023-03-18 14:11:00 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,8 +19,10 @@ int main(void)
 			putchar(j % 10 + '0');
 
 			if (i != 98 || j != 99)
-			putchar(',');
-			putchar(' ');
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
