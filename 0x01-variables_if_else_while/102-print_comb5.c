7955b9053e3019745203689abe91a9a361e333c2
Karilaw <lawrencekariithi6@gmail.com>
2023-03-18 14:20:50 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
@@ -25,6 +25,7 @@ int main(void)
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
