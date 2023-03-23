67353b19788559e360efb661550a77ecd5ad9800
Karilaw <lawrencekariithi6@gmail.com>
2023-03-23 16:19:32 -0700

    First commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* fizz_buzz.c - A program that prints Fizz Buzz */
+/**
+ * main - Prints Fizz Buzz from 1 to 100
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
