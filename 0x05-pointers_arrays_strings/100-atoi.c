99254ceb57190b10705949559e1ac76da3ae345c
Karilaw <lawrencekariithi6@gmail.com>
2023-03-27 12:12:22 -0700

    Commit for Test 2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
@@ -19,6 +20,8 @@ int _atoi(char *s)
 	}
 	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
 	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			return (sign == 1 ? INT_MAX : INT_MIN);
 		result = result * 10 + s[i] - '0';
 		i++;
 	}
