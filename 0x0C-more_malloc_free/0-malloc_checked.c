5107e1529fc3407b388d2e8f2364ef3e2b7fb2e9
Karilaw <lawrencekariithi6@gmail.com>
2023-04-13 03:59:02 -0700

    First Commit to Test

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: the number of bytes
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
