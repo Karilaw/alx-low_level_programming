f5e4449254faf8fe357533bb7cb9f9ecef2a516c
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-04-27 03:57:48 -0700

    Adding All Files for Checking

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Print list element
+ * @h: pointer
+ * Return: The number of element
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+			printf("[%u]%s\n", h->len, h->str);
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
