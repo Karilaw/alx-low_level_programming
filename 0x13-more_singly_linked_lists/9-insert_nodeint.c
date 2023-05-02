cda1f8f23b371df37338a5d46986a739fb294502
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-05-02 04:58:20 -0700

    First commit to test quiz 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts data at particular index
+ * @head: pointer to pointer of first node
+ * @idx: index to insert data
+ * @n: data to be inserted
+ * Return: memory address of inserted data
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp;
+	listint_t *new_node;
+	unsigned int i = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	temp = *head;
+	for (i = 0; i < idx -1; i++)
+	{
+		if (temp == NULL || temp->next == NULL)
+		{
+			return (NULL);
+		}
+		temp = temp->next;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
