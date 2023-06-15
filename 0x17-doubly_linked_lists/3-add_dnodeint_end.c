1c29b31bf95f98b89943f228beb483620ea1211d
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-15 07:39:08 -0700

    first commit to test to test

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -17,10 +17,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node = NULL;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
-		new_node = NULL;
+		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
