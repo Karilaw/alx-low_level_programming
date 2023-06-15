3ea1f763c5fb485e06224e8b03d062e6215780c3
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-15 08:04:28 -0700

    first commit to test

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: pointer to head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current, *next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
