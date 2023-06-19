29693c0ae3ade988095a7b65c4df3af433b84bbf
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-06-19 04:40:15 -0700

    first commit to test

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value of string
+ */
+
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+			return (sign == 1 ? INT_MAX : INT_MIN);
+		result = result * 10 + s[i] - '0';
+		i++;
+	}
+
+	return (sign * result);
+}
