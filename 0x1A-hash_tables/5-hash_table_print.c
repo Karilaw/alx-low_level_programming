b4ba1bcefa6bd3a379bdeb8aad577906aaf34131
Lawrence Wangare <lawrencekariithi6@gmail.com>
2023-07-07 07:30:44 -0700

    first commit to test

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table to print
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (first == 0)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
