c5c25b7df45cb633a36a46cf9c2c859a12c49173
Karilaw <lawrencekariithi6@gmail.com>
2023-10-11 03:26:35 -0700

    committing tasks to test

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,10 +11,10 @@ void print_array(int *array, size_t start, size_t end)
 	size_t i;
 
 	printf("Searching in array: ");
-	for (i = start; i < end; i++)
+	for (i = start; i <= end; i++)
 	{
 		printf("%d", array[i]);
-		if (i + 1 != end)
+		if (i != end)
 			printf(", ");
 	}
 	printf("\n");
@@ -32,20 +32,20 @@ void print_array(int *array, size_t start, size_t end)
 int binary_search(int *array, size_t size, int value)
 {
 	size_t start = 0;
-	size_t end = size;
+	size_t end = size - 1;
 	size_t mid;
 
 	if (!array)
 		return (-1);
 
-	while (start < end)
+	while (start <= end)
 	{
 		print_array(array, start, end);
-		mid = (start + end) / 2;
+		mid = start + (end - start) / 2;
 		if (array[mid] < value)
 			start = mid + 1;
 		else if (array[mid] > value)
-			end = mid;
+			end = mid - 1;
 		else
 			return (mid);
 	}
